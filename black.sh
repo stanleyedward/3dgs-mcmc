fdd9e493cb49b4da310e5fbf2cd3989744806210
stanleyedward <114278820+stanleyedward@users.noreply.github.com>
2025-07-24 17:06:00 +0530

    update bash script

--- black.sh
+++ black.sh
@@ -3,12 +3,17 @@ RESULT_DIR="results/black_v1"
 SCENE_LIST="garden"
 CAP_MAX=1000000
 ITERATION=30000
-BLACK_ARGS="--black_iter 20_000 --black_gradient_threshold 0.0002"
+BLACK_ARGS="--black_iter 20_000"
+DATA_FACTOR=4
+BLACK_THRESHOLD="0.0002 0.002 0.0001"
 
 for SCENE in $SCENE_LIST;
 do
-    echo "Running $SCENE"
-        python train.py -s $SCENE_DIR/$SCENE --cap_max $CAP_MAX -m $RESULT_DIR/$SCENE -r $DATA_FACTOR --iterations $ITERATION --outdoor $BLACK_ARGS
-        python render.py --iteration $ITERATION -s $SCENE_DIR/$SCENE -m $RESULT_DIR/$SCENE --eval --skip_train -r $DATA_FACTOR
-        # python metrics.py -m $RESULT_DIR/$SCENE
+    for THRESHOLD in $BLACK_THRESHOLD;
+    do
+        echo "Running $SCENE with threshold $THRESHOLD"
+            python train.py -s $SCENE_DIR/$SCENE --cap_max $CAP_MAX -m $RESULT_DIR/$SCENE -r $DATA_FACTOR --iterations $ITERATION --outdoor $BLACK_ARGS \
+            --black_threshold $THRESHOLD 
+            python render.py --iteration $ITERATION -s $SCENE_DIR/$SCENE -m $RESULT_DIR/$SCENE --eval --skip_train -r $DATA_FACTOR
+    done
 done
